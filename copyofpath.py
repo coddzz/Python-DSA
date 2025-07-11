e200c0a536a96f8fa90ebb5f326c1b4d7ca0dcfb
Sarath <learn.cypher@gmail.com>
2025-07-11 17:03:49 +0530

    Backtracking

---
+++ copyofpath.py
@@ -0,0 +1,5 @@
+cnt = [1,2,3]
+path = cnt
+copyofpath = path[:]
+print(copyofpath is path)
+print(path is cnt)
---
+++ forloop.py
@@ -0,0 +1,6 @@
+nums = [5,6,7]
+path = []
+for i in range(0, len(nums)):
+    path.append(nums[i])
+    print(path)
+print(path)
