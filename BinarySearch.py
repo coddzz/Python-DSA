3fddfd15252a99e00208730a59f5c8faf47d87c6
Sarath <learn.cypher@gmail.com>
2025-07-22 02:05:03 +0530

    BinarySearch.py

---
+++ BinarySearch.py
@@ -0,0 +1,13 @@
+def binary_search(arr, target):
+    low, high = 0, len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+print(binary_search([1, 2, 3, 4, 5], 4))  # Output: 3
